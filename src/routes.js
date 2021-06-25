03113b98861ce5a032e94a3e7248e24d8fdaf526
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-06-25 18:59:45 -0300

    Separei as rotas e criei o Controller da home.

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+const Router = require('koa-router');
+const router = new Router();
+const homeController = require('./controllers/homeController');
+
+router.get('/', homeController.paginaInicial);
+
+module.exports = router;
